f6a54615885bf79e965c5874c967ccdb3ba0f19a
Banti Kumar Singh <bantikumarsingh91@gmail.com>
2025-01-10 13:43:45 +0530

    add github link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import style from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={style.footer}>
-        <p>built with <GiCoffeeCup/>  by <a href="https://www.linkedin.com/in/banti-kr-singh/" target='_blank'>Bunny</a> | <a href="">&lt;/src code&gt;</a></p>
+        <p>built with <GiCoffeeCup/>  by <a href="https://www.linkedin.com/in/banti-kr-singh/" target='_blank'>Bunny</a> | <a href="https://github.com/BunniSingh/Trello-Clone_fs23" target='_blank'>&lt;/src code&gt;</a></p>
     </div>
   )
 }
